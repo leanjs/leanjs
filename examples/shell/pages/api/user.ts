ed43cff8a2f7e218693e66d9b118158fa2a7c1d4
Alex Lobera <alex.lobera@gmail.com>
2022-03-25 12:09:35 +0000

    update examples

--- examples/shell/pages/api/user.ts
+++ examples/shell/pages/api/user.ts
@@ -1,3 +1,30 @@
-export default function handler(req, res) {
-  res.status(200).json({ username: "Alex" });
+import Cors from "cors";
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
 }
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
+  res.json({ username: "@alex_lobera" });
+}
+
+export default handler;
