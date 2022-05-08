a50ac64dbc1d48d1059145fbd6b03c6620cd1d4d
Alex Lobera <alex.lobera@gmail.com>
2022-05-08 22:15:10 +0100

    feat(core): add utils and updates types

---
+++ packages/core/src/utils/loadScript.ts
@@ -0,0 +1,19 @@
+export const loadScript = (src: string) =>
+  new Promise<void>((resolve, reject) => {
+    const element = document.createElement("script");
+    const { head } = document;
+    element.src = src;
+    element.async = true;
+
+    element.onload = () => {
+      head.removeChild(element);
+      resolve();
+    };
+
+    element.onerror = () => {
+      head.removeChild(element);
+      reject(new Error(`Failed to load script ${src}`));
+    };
+
+    head.appendChild(element);
+  });
