b53f435d5cfdc89f3f95c3e7180cd09fce3ba0af
Alex Lobera <alex.lobera@gmail.com>
2022-07-17 18:22:36 +0100

    chore(website): add docusaurus website

---
+++ website/docusaurus.config.js
@@ -0,0 +1,154 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "LeanJS",
+  tagline: "Tools for building maintainable and performant micro-frontends ",
+  url: "https://leanjs.org",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "leanjs", // Usually your GitHub org/user name.
+  projectName: "leanjs", // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          editUrl: "https://github.com/leanjs/leanjs/edit/main/docs/",
+          path: "../docs",
+          routeBasePath: "/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "packages",
+        breadcrumbs: false,
+        path: "../packages",
+        routeBasePath: "packages",
+        include: ["**/*.md", "**/*.mdx"],
+        exclude: [
+          "**/_*.{js,jsx,ts,tsx,md,mdx}",
+          "**/_*/**",
+          "**/*.test.{js,jsx,ts,tsx}",
+          "**/CHANGELOG.md",
+          "**/__tests__/**",
+          "**/node_modules/**",
+        ],
+        sidebarItemsGenerator: async (
+          /** @type {{ docs: { id: string; sourceDirName: string; }[]; }} */ items
+        ) => {
+          return items.docs
+            .filter((a) => a.id !== "runtime/README")
+            .sort((a, b) =>
+              a.sourceDirName < b.sourceDirName
+                ? -1
+                : a.sourceDirName > b.sourceDirName
+                ? 1
+                : 0
+            )
+            .map(({ id, sourceDirName }) => ({
+              label: sourceDirName,
+              id,
+              type: "doc",
+            }));
+        },
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "LeanJS",
+        // logo: {
+        //   alt: "My Site Logo",
+        //   src: "img/logo.svg",
+        // },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            to: "/packages/create-micro-frontends",
+            label: "Packages",
+            position: "left",
+          },
+          {
+            href: "https://github.com/leanjs/leanjs",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Twitter",
+                href: "https://twitter.com/leanjs",
+              },
+            ],
+          },
+          {
+            title: "Code",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/leanjs/leanjs",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                href: "https://alexlobera.com",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} LeanJS.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
