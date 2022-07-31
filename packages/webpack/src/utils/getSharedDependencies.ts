443102a75e35c71bfce299bca0fe61cdbd79d238
Alex Lobera <alex.lobera@gmail.com>
2022-07-31 22:18:40 +0100

    fix(webpack): host shared version merges implicit shared versions

--- packages/webpack/src/utils/getSharedDependencies.ts
+++ packages/webpack/src/utils/getSharedDependencies.ts
@@ -13,19 +13,19 @@ export function getSharedDependencies({
   dependencies,
   peerDependencies,
 }: GetSharedDependencies) {
-  // We run execFileSync because we use this function inside Webpack `apply` method which is synchronous,
-  // and here we want to asynchronously read multiple files if we are in a monorepo.
-  // We are not using async Webpack hooks because there is no async hook to modify
-  // the Webpack config before ModuleFederationPlugin is applied (as far as I know).
-  // To improve performance, we asynchronously read files in the repo inside the following script.
-  // We need to synchronously wait for the end of the execution of the script because of this sync `apply` method
-  const monorepoVersions = execFileSync("node", [
-    `${__dirname}/../scripts/stdoutWriteMonorepoVersions.js`,
-  ]);
-
-  let sharedDependencies: DependencyVersion = {};
+  let monorepoDependencies: DependencyVersion = {};
   try {
-    sharedDependencies = JSON.parse(monorepoVersions.toString());
+    monorepoDependencies = JSON.parse(
+      // We run execFileSync because we use this function inside Webpack `apply` method which is synchronous,
+      // and here we want to asynchronously read multiple files if we are in a monorepo.
+      // We are not using async Webpack hooks because there is no async hook to modify
+      // the Webpack config before ModuleFederationPlugin is applied (as far as I know).
+      // To improve performance, we asynchronously read files in the repo inside the following script.
+      // We need to synchronously wait for the end of the execution of the script because of this sync `apply` method
+      execFileSync("node", [
+        `${__dirname}/../scripts/stdoutWriteMonorepoVersions.js`,
+      ]).toString()
+    );
   } catch (error) {
     console.log(
       `🔥 Failed to read shared dependencies of ${packageName}. Building with no shared dependencies`,
@@ -36,6 +36,6 @@ export function getSharedDependencies({
   return versionDependencies({
     dependencies,
     peerDependencies,
-    sharedDependencies,
+    monorepoDependencies,
   });
 }
