861780c1075d3880c122bb1eb7f446b9187ed9cb
Alex Lobera <alex.lobera@gmail.com>
2022-10-10 23:13:11 +0100

    chore: updates examples

--- examples/coolest-todos/micro-frontends/feed/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "@leanjs/react-router";
-
-import packageJson from "../package.json";
-import { FeedApp } from "./components/FeedApp";
-
-export default createApp(FeedApp, {
-  packageName: packageJson.name,
-});
--- examples/coolest-todos/micro-frontends/todo/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "@leanjs/vue-router";
-
-import TodoApp from "./components/TodoApp.vue";
-import packageJson from "../package.json";
-
-export default createApp(TodoApp, {
-  packageName: packageJson.name,
-});
