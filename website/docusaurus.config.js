c9cf496b382bbe7b3a4118421d5e939dd2260277
Alex Lobera <alex.lobera@gmail.com>
2022-10-16 16:01:16 +0100

    chore: deploy website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -17,8 +17,8 @@ const config = {
   favicon: "img/favicon.ico",
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "leanjs", // Usually your GitHub org/user name.
-  projectName: "leanjs", // Usually your repo name.
+  organizationName: "leanjs",
+  projectName: "leanjs",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
