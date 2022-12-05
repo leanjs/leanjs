6694174e8f0becec17f2f04cfdb7204cee0a49b0
Alex Traher <alex.traher@gmail.com>
2022-12-05 19:40:51 +0000

    chore(update-nuxt-version): update to stable version of nuxt 3

--- e2e/test-subjects/shell-nuxt-3/nuxt.config.ts
+++ e2e/test-subjects/shell-nuxt-3/nuxt.config.ts
@@ -1,14 +1,6 @@
-import { defineNuxtConfig } from "nuxt3";
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  vite: false,
-  vue: {
-    config: {
-      productionTip: false,
-      devtools: true,
-    },
-  },
+  builder: "webpack",
   build: {
     transpile: [
       "@leanjs/e2e-test-package-runtime-shared",
